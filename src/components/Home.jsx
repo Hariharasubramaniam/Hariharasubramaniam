6c7b58b7ea86d1c9ac18173e20b3d03d1a327c50
hari <hariaum2003@gmail.com>
2023-10-14 01:07:40 +0530

    Corrected the issue with the photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Typed from "react-typed";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import MyPhoto from "../assets/Photo.jpeg";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#0a185f]">
-      {/* Container */}
-      <div className="max-w-[1080px] mx-auto px-8 flex flex-col justify-center h-full">
+    <div className="w-full h-screen bg-[#0a185f] flex justify-center items-center">
+      <div className="text-center">
         <p className="text-orange-600 text-3xl font-semibold">Hi, my name is</p>
-        <h1 className="animate-pulse bg-gradient-to-r from-[#9fb2eb] via-yellow-500 to-violet-700 bg-clip-text text-transparent text-4xl my-2 sm:text-5xl font-bold">
+        <h1 className="animate-pulse bg-gradient-to-r from-[#6dfff3] to-red-500 bg-clip-text text-transparent text-4xl my-2 sm:text-5xl font-bold">
           S.Hariharasubramaniam
         </h1>
         <div>
@@ -36,13 +36,16 @@ const Home = () => {
           development🤩
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex item-center hover:bg-orange-600 hover:border-orange-600">
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600">
             View work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="flex ml-3 justify-center items-center" />
             </span>
           </button>
         </div>
+      </div>
+      <div className="rounded-full border-4 border-[#0a185f] p-2 glowing-border">
+        <img className="w-80 rounded-full" src={MyPhoto} alt="My Photo" />
       </div>
     </div>
   );
