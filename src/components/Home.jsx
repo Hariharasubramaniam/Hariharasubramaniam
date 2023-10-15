a764d80a526eff8156e0d8267be1e49d1783f1a1
hari <hariaum2003@gmail.com>
2023-10-15 10:20:23 +0530

    Photo styling changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ import MyPhoto from "../assets/Photo.jpeg";
 const Home = () => {
   return (
     <div className="w-full h-screen bg-[#0a185f] flex justify-center items-center">
-      <div className="text-center">
+      <div className="text-left">
         <p className="text-orange-600 text-3xl font-semibold">Hi, my name is</p>
-        <h1 className="animate-pulse bg-gradient-to-r from-[#6dfff3] to-red-500 bg-clip-text text-transparent text-4xl my-2 sm:text-5xl font-bold">
+        <h1 className="animate-pulse bg-gradient-to-r from-[#81a0ff] to-yellow-500 bg-clip-text text-transparent text-4xl my-2 sm:text-5xl font-bold">
           S.Hariharasubramaniam
         </h1>
         <div>
@@ -32,8 +32,8 @@ const Home = () => {
           </h2>
         </div>
         <p className="text-orange-600 py-4 max-w-[700px] text-[21px]">
-          I have a strong hand in Music and very much interested in front-end
-          development🤩
+          I have a strong hand in Music.<br></br>Very much interested in
+          front-end development🤩
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600">
@@ -44,8 +44,12 @@ const Home = () => {
           </button>
         </div>
       </div>
-      <div className="rounded-full border-4 border-[#0a185f] p-2 glowing-border">
-        <img className="w-80 rounded-full" src={MyPhoto} alt="My Photo" />
+      <div className="rounded-full border-[#46179c] p-2 glowing-border">
+        <img
+          className="w-80 rounded-full shadow-[0_0_75px_theme('colors.blue.500'),0_0_50px_theme('colors.violet.700'),0_0_75px_theme('colors.red.500')]"
+          src={MyPhoto}
+          alt="My Photo"
+        />
       </div>
     </div>
   );
