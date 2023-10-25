37c3a78714799a924fdf77a9adb34a640f10daba
hari <hariaum2003@gmail.com>
2023-10-25 23:26:44 +0530

    Worked on work..

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+// import Sleep from "../assets/SleepECG.png";
+import HandImg from "../assets/Hand.jpg";
+// import Todo from "../assets/Todo.jpeg";
 
 const Work = () => {
   return (
@@ -10,17 +13,19 @@ const Work = () => {
           </p>
           <p className="py-6">Check out some of my works.</p>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div
+          style={{ backgroundImage: `url(${HandImg})` }}
+          className="grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
           <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center item-center mx-auto content-div">
-            {/* Hover */}
-            <div>
-              <span></span>
+            {/* Hover Effects*/}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Hand Gesture Recognition
+              </span>
               <div>
                 <a href="/">
-                  <button></button>
-                </a>
-                <a href="/">
-                  <button></button>
+                  <button>Code</button>
                 </a>
               </div>
             </div>
