36b1d6014b169ed9117acb761e66fcfead4524ca
hari <hariaum2003@gmail.com>
2023-10-31 22:39:15 +0530

    Cheap commits😬

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-center pb-8 pl-4">
-            <p className="text-gray-300 animate-pulse bg-clip-text text-transparent text-4xl font-bold inline border-b-4 border-orange-600">
+            <p className="text-gray-300 animate-pulse text-4xl font-bold inline border-b-4 border-orange-600">
               About
             </p>
           </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="animate-pulse text-gray-300 bg-clip-text text-transparent text-4xl font-bold inline border-b-4 border-orange-600">
+          <p className="animate-pulse text-gray-300 text-4xl font-bold inline border-b-4 border-orange-600">
             Experience
           </p>
           <p className="text-xl py-4">
