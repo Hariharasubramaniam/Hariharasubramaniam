d9bd71b3e504bc85431f6e038570807b01257091
hari <hariaum2003@gmail.com>
2023-11-08 12:55:44 +0530

    Tried adding parallax scrolling animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,22 +2,21 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-[#0a185f] text-[#8892b0]">
+    <div name="about" className="w-full bg-[#98a7f0] text-[#372272]">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+        <div className="max-w-[1000px] w-full h-[140px] grid grid-cols-2 gap-8">
           <div className="sm:text-center pb-8 pl-4">
-            <p className="text-gray-300 animate-pulse text-4xl font-bold inline border-b-4 border-orange-600">
+            <p className="text-black animate-pulse text-4xl font-bold inline border-b-4 border-orange-600">
               About
             </p>
           </div>
-          <div></div>
         </div>
         <div className="max-w-[1100px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
             <p>Hi. I'm Hari, nice to meet you. Please take a look around.</p>
           </div>
           <div>
-            <p className="text-xl text-orange-500">
+            <p className="text-xl text-red-700">
               I'm a 3rd year Artificial Intelligence student at Amrita
               University. Being naturally talented in Music🎙️, exploring deep
               into the world of software development🤩 and AI Always open for
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,57 +1,68 @@
 import React from "react";
 import Typed from "react-typed";
+import Bg from "../assets/Background.jpg";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import MyPhoto from "../assets/Photo.jpeg";
+import { Parallax } from "react-parallax";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#0a185f] flex justify-center items-center">
-      <div className="text-left pr-20">
-        <p className="text-orange-500 text-3xl font-semibold">Hi, my name is</p>
-        <h1 className="bg-gradient-to-r from-[#6cffdfe7] to-yellow-400 bg-clip-text text-transparent text-4xl my-2 sm:text-5xl font-bold">
-          S.Hariharasubramaniam
-        </h1>
-        <div>
-          <h2 className="text-4xl my-2 sm:text-5xl font-bold text-[#9fb2eb]">
-            I'm into&nbsp;
-            <Typed
-              strings={["Music", "Sports", "Gaming"]}
-              typeSpeed={60}
-              backSpeed={30}
-              loop={true}
-            />
-          </h2>
-          <h2 className="text-4xl sm:text-5xl my-2 font-bold text-[#e6c94b]">
-            Learning &nbsp;
-            <Typed
-              strings={["Web-Dev", "Deep Learning"]}
-              typeSpeed={60}
-              backSpeed={30}
-              loop={true}
-            />
-          </h2>
+    <Parallax
+      className="image"
+      blur={{ min: -1, max: 1 }}
+      bgImage={Bg}
+      strength="1000"
+    >
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="text-left pr-20">
+          <p className="text-orange-500 text-3xl font-semibold">
+            Hi, my name is
+          </p>
+          <h1 className="bg-gradient-to-r from-[#6cffdfe7] to-yellow-400 bg-clip-text text-transparent text-4xl my-2 sm:text-5xl font-bold">
+            S.Hariharasubramaniam
+          </h1>
+          <div>
+            <h2 className="text-4xl my-2 sm:text-5xl font-bold text-[#9fb2eb]">
+              I'm into&nbsp;
+              <Typed
+                strings={["Music", "Sports", "Gaming"]}
+                typeSpeed={60}
+                backSpeed={30}
+                loop={true}
+              />
+            </h2>
+            <h2 className="text-4xl sm:text-5xl my-2 font-bold text-[#e6c94b]">
+              Learning &nbsp;
+              <Typed
+                strings={["Web-Dev", "Deep Learning"]}
+                typeSpeed={60}
+                backSpeed={30}
+                loop={true}
+              />
+            </h2>
+          </div>
+          <p className="text-orange-500 py-4 max-w-[700px] text-[21px]">
+            I have a strong hand in Music.<br></br>Very much interested in
+            front-end development🤩
+          </p>
+          <div>
+            <button className="text-white rounded-lg group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600">
+              View work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="flex justify-center items-center" />
+              </span>
+            </button>
+          </div>
         </div>
-        <p className="text-orange-500 py-4 max-w-[700px] text-[21px]">
-          I have a strong hand in Music.<br></br>Very much interested in
-          front-end development🤩
-        </p>
-        <div>
-          <button className="text-white rounded-lg group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600">
-            View work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="flex justify-center items-center" />
-            </span>
-          </button>
+        <div className="rounded-full border-[#46179c] p-2 glowing-border">
+          <img
+            className="w-80 rounded-full shadow-[0_0_75px_theme('colors.blue.500'),0_0_50px_theme('colors.violet.700'),0_0_75px_theme('colors.red.500')]"
+            src={MyPhoto}
+            alt="My Photo"
+          />
         </div>
       </div>
-      <div className="rounded-full border-[#46179c] p-2 glowing-border">
-        <img
-          className="w-80 rounded-full shadow-[0_0_75px_theme('colors.blue.500'),0_0_50px_theme('colors.violet.700'),0_0_75px_theme('colors.red.500')]"
-          src={MyPhoto}
-          alt="My Photo"
-        />
-      </div>
-    </div>
+    </Parallax>
   );
 };
 
