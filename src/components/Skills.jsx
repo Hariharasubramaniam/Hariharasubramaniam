28d033130c222b615188fce44bc54a6ff4a2f85a
hari <hariaum2003@gmail.com>
2023-11-12 18:58:19 +0530

    Edited Experiences

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Parallax } from "react-parallax";
-import Bgskills from "../assets/SkillsBG.jpg";
+import SkillsBG from "../assets/SkillsBG.mp4";
+import { motion } from "framer-motion";
 
 import Java from "../assets/java.png";
 import Python from "../assets/py.png";
@@ -14,9 +15,28 @@ import Firebase from "../assets/Firebase.png";
 
 const Skills = () => {
   return (
-    <Parallax className="image" bgImage={Bgskills} strength="1000">
-      <div name="skills" className="w-full h-screen text-[#8892b0]">
-        {/* Container */}
+    <div style={{ position: "relative" }}>
+      <Parallax bgImage={SkillsBG} strength={1000}>
+        <video
+          autoPlay
+          loop
+          muted
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        >
+          <source src={SkillsBG} type="video/mp4" />
+        </video>
+      </Parallax>
+      <div
+        name="skills"
+        className="w-full h-full text-[#8892b0]"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: 1,
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        }}
+      >
         <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
           <div>
             <p className="animate-pulse text-gray-300 text-4xl font-bold inline border-b-4 border-orange-600">
@@ -26,19 +46,31 @@ const Skills = () => {
               I can handle the following languages and frameworks.
             </p>
           </div>
-
           <div className="w-full grid grid-col-1 sm:grid-cols-3 gap-4 text-center py-8">
             <div className="shadow-md shadow-[040c16] hover:scale-110 duration-500">
-              <img className="w-20 mx-auto" src={Java} alt="Java icon" />
-              <p className="my-[16.5px] text-2xl font-bold ">JAVA</p>
+              <img
+                className="w-20 mx-auto scale-110"
+                src={Java}
+                alt="Java icon"
+              />
+              <p className="my-[15.1px] text-2xl font-bold ">JAVA</p>
             </div>
-            <div className="shadow-md shadow-[040c16] hover:scale-110 duration-500">
-              <img className="w-20 mx-auto" src={Python} alt="Python icon" />
-              <p className="my-[45px] text-2xl font-bold ">PYTHON</p>
+
+            <div className="mt-[20.5px] shadow-md shadow-[040c16] hover:scale-110 duration-500">
+              <img
+                className="w-20 mx-auto scale-125"
+                src={Python}
+                alt="Python icon"
+              />
+              <p className="my-[23px] text-2xl font-bold ">PYTHON</p>
             </div>
-            <div className="shadow-md shadow-[040c16] hover:scale-110 duration-500">
-              <img className="w-20 mx-auto" src={HTML} alt="Html icon" />
-              <p className="my-[48px] text-2xl font-bold ">HTML</p>
+            <div className="mt-6 shadow-md shadow-[040c16] hover:scale-110 duration-500">
+              <img
+                className="w-20 mx-auto scale-125"
+                src={HTML}
+                alt="Html icon"
+              />
+              <p className="my-[22px] text-2xl font-bold ">HTML</p>
             </div>
           </div>
           <div className="w-full grid grid-col-1 sm:grid-cols-3 text-center py-8">
@@ -79,7 +111,7 @@ const Skills = () => {
           </div>
         </div>
       </div>
-    </Parallax>
+    </div>
   );
 };
 
